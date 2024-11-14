bbdd22972723b02327b3e5e31ee8cf72aba1fe9c
TanishqAaryan <tanishq.aaryan@rsystems.com>
2024-11-14 05:20:01 -0800

    Add files via upload
    1. added 3 more requirement
    2. added a new module for images in app.py
    3. changed streamlit_app.py so that it may accept image as input

--- app.py
+++ app.py
@@ -2,18 +2,16 @@ from flask import Flask
 from routes.company_routes import company_bp
 from routes.ad_routes import ad_bp
 from routes.analytics_routes import analytics_bp
+from routes.img_routes import img_bp
 from routes.llm_routes import llm_bp
-from flask_cors import CORS
 
 app = Flask(__name__)
-
-# Allow all origins (you can restrict this if needed)
-CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins, or replace "*" with "http://localhost:3000"
 
 # Register blueprints
 app.register_blueprint(company_bp)
 app.register_blueprint(ad_bp)
 app.register_blueprint(analytics_bp)
+app.register_blueprint(img_bp)
 app.register_blueprint(llm_bp)
 
 if __name__ == "__main__":
